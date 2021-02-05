1d44e8b2a29cea29def54b73392158e1d0bbe45f
fimion <fimion@gmail.com>
2021-02-05 10:23:01 -0500

    Start to mess with fonts

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-  <h1>GitHubCities</h1>
-  <SearchBar @submit="triggerQuery"/>
-  <div v-if="status === FETCH_STATUS.RESOLVED">
-    <h2>Results ({{ results.userCount }})</h2>
-    <ul class="user-list">
-      <li class="user" v-for="user in results.users"
-          :key="user.id">
-        <a class="user-box"
-           :href="user.url">
-          <img class="avatar"
-               :src="user.avatarUrl"
-               :alt="`avatar for ${user.name}`">
-          {{ user.name }} (@{{user.login}})
-        </a>
-      </li>
-    </ul>
-  </div>
-  <pre v-if="status===FETCH_STATUS.ERROR">
-    <h2>Oh no! :(</h2>
+  <div class="githubcities">
+    <h1>GitHubCities</h1>
+    <SearchBar @submit="triggerQuery"/>
+    <template v-if="status === FETCH_STATUS.RESOLVED">
+      <h2>Results ({{ results.userCount }})</h2>
+      <ul class="user-list">
+        <li class="user" v-for="user in results.users"
+            :key="user.id">
+          <a class="user-box"
+             :href="user.url">
+            <img class="avatar"
+                 :src="user.avatarUrl"
+                 :alt="`avatar for ${user.name}`">
+            {{ user.name }} (@{{ user.login }})
+          </a>
+        </li>
+      </ul>
+    </template>
+    <template v-if="status===FETCH_STATUS.ERROR">
+      <h2>Oh no! :(</h2>
+      <pre>
     {{ results }}
-  </pre>
+    </pre>
+    </template>
+  </div>
 </template>
 <script>
 import SearchBar from "./components/SearchBar.vue"
@@ -45,20 +49,19 @@ export default defineComponent({
     const query = ref('')
     const status = ref(FETCH_STATUS.READY)
     const results = ref({})
-    const cursor = ref(null);
+    const cursor = ref(null)
 
 
     const searchUrl = computed(() => {
       const q = encodeURIComponent(query.value)
-      const cur = cursor.value ? `&cursor=${cursor.value}` : '';
+      const cur = cursor.value ? `&cursor=${cursor.value}` : ''
       return `/api/search?q=${q}${cur}`
-  });
+    })
 
 
-
-    const callSearch = async ()=>{
+    const callSearch = async () => {
       try {
-        results.value = await fetchWrapper(searchUrl.value);
+        results.value = await fetchWrapper(searchUrl.value)
         status.value = FETCH_STATUS.RESOLVED
       } catch (e) {
         status.value = FETCH_STATUS.ERROR
@@ -73,18 +76,17 @@ export default defineComponent({
      */
     const triggerQuery = async (q) => {
       if (q !== '') {
-        cursor.value=null;
+        cursor.value = null
         status.value = FETCH_STATUS.LOADING
         query.value = q
-        return await callSearch();
+        return await callSearch()
       }
     }
 
-    const changePage = async (cur)=>{
-      cursor.value = cur;
-      return await callSearch();
+    const changePage = async (cur) => {
+      cursor.value = cur
+      return await callSearch()
     }
-
 
 
     return {
@@ -101,27 +103,38 @@ export default defineComponent({
 
 <style>
 
-*{
-  padding:0;margin:0;box-sizing: border-box;
-}
-:root{
-  --min-user-width:100px;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Pangolin', cursive;
 }
 
-.user-list{
-  display:grid;
+:root {
+  --min-user-width: 100px;
+}
+
+.githubcities{
+  display:flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
+
+.user-list {
+  display: grid;
   max-width: 100%;
-  grid-gap:10px;
+  grid-gap: 10px;
   grid-template-columns: minmax(var(--min-user-width), auto) minmax(var(--min-user-width), auto) minmax(var(--min-user-width), auto);
 }
 
-.user{
-  display:inline-block;
+.user {
+  display: inline-block;
   margin: 10px;
 }
 
-.user-box{
-  display:flex;
+.user-box {
+  display: flex;
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
@@ -129,7 +142,7 @@ export default defineComponent({
 
 }
 
-.avatar{
+.avatar {
   max-width: var(--min-user-width);
   border-radius: 50%;
   border: seagreen 5px solid;
